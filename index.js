3baae743aad92db8f4a35aceaf3a13eb9ebdfca4
Hector Morales <tuntu99@gmail.com>
2021-09-20 18:39:17 -0600

    Agregado el profile name

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+//Obtener variables de entorno
 const { config } = require("./config/index");
+//Declaracion de librerias que se estan utilizando
 const uuid = require("uuid");
 const sessionClient = require("./lib/DialogFlow");
 const express = require("express");
@@ -6,15 +8,17 @@ const cors = require("cors");
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
 const bodyParser = require('body-parser')
 
+//Configuracion del servidor
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+//Funcion que detenta los intents de Dialogflow, tiene como parametro query, que es el mensaje que recibe
 async function detectIntent(query) {
   // The path to identify the agent that owns the created intent.
   const uniqueID = uuid.v4();
-
   const sessionPath = sessionClient.projectAgentSessionPath(
     config.project_id,
     uniqueID
@@ -25,6 +29,7 @@ async function detectIntent(query) {
     session: sessionPath,
     queryInput: {
       text: {
+        //Uso del mensaje que recibe
         text: query,
         languageCode: "es",
       },
@@ -35,14 +40,18 @@ async function detectIntent(query) {
   return responses[0];
 }
 
+//Verificar que el servidor esta activo
 app.get("/", (req, res) => {
   res.send("Todo gucci");
 });
 
 app.post("/api/inbound-message", (req, res) => {
   const twiml = new MessagingResponse();
+  
 
+  //Asignar a message el body del request
   const message = req.body.Body;
+  const customerName = req.body.ProfileName
   
   detectIntent(message)
     .then((intent) => {
@@ -50,13 +59,14 @@ app.post("/api/inbound-message", (req, res) => {
       const intentName = result.intent.displayName;
 
       if (intentName === "Default Fallback Intent") {
-        twiml.message(`Bienvenido al servicio de rastreo de encomiendas de Transportes Castillo.
+        twiml.message(`Bienvenido ${customerName} al servicio de rastreo de encomiendas de Transportes Castillo.
         Seleccione la opción que sea de sus interes:
         1️⃣ CDM
         2️⃣ VVV
         3️⃣ HDTP
         4️⃣ ALC
         5️⃣ ALV`);
+        //Lineas requeridas despues de cada respuesta
         res.writeHead(200, { "Content-Type": "text/xml" });
         res.end(twiml.toString());
       } else if (intentName === "laconcha") {
